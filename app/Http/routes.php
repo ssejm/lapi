fed64404f1c636c3dbe7c08943c3cee44c149655
J M <partlysunny16@yahoo.com>
2015-04-21 23:51:01 -0700

    added first mvc

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,13 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'WelcomeController@index');
+//
+//Route::get('home', 'HomeController@index');
+//
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
 
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('/{name?}', 'MyController@index');
